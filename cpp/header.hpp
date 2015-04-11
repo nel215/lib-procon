df94b23273c8c8e8438957463b7226f60bd470ea
nel215 <otomo.yuhei@gmail.com>
2015-04-11 15:49:51 +0900

    add header.hpp

---
+++ cpp/header.hpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <complex>
+#include <algorithm>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+
+#define REP(i,x) for(int i=0 ; i<(int)(x) ; i++)
+#define ALL(x) (x).begin(),(x).end()
+#define LL long long
+
+using namespace std;
